e4f96d3f805e11df1420dea58aba9c90e88822d5
rafael <nomar22@gmail.com>
2018-07-05 21:08:03 -0300

    created all actions and redux, not connected yet

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import { ADD_PLACE, DELETE_PLACE, DESELECT_PLACE, selectedPlace, SELECT_PLACE } from '../actions/actionTypes'
+import image from '../../assets/lisbon.jpg';
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,9 +7,38 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat(
+                    {
+                        key: Math.random().toString(),
+                        placeName: action.placeName,
+                        image
+                    })
+            };
 
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(({ key }) => {
+                    return key != action.key;
+                }
+                )
+            }
 
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(({ key }) => {
+                    return key === action.key;
+                })
+            }
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            }
         default:
             return state;
     }
