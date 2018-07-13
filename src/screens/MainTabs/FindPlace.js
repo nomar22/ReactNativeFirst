ac924def9f1d0667fd5f6ce8afb58c40ca86ec05
rafael <nomar22@gmail.com>
2018-07-12 21:09:19 -0300

    Revert "remove all"
    This reverts commit fd3d8f164574824d35ee6a4cfab52226540768cf.

--- src/screens/MainTabs/FindPlace.js
+++ src/screens/MainTabs/FindPlace.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import List from '../../components/List';
-import { deletePlace } from '../../store/actions/index';
-import { deleteAll } from '../../store/actions/places';
+import { deletePlace } from '../../store/actions/index'
 
 class FindPlaceScreen extends React.Component {
-    isNotEmpty = () => this.props.places.length > 0
-
     render() {
         return (
-            <View >
-               { this.isNotEmpty()?(
-                <Button 
-                    title="Remove All" 
-                    color="#841584"
-                    onPress={this.props.onDeleteAll} />)
-                    :(null)
-                }
-                
+            <View>
                 <List
                     items={this.props.places}
                     showItemHandler={(id) => alert(id)}
                     removeHandler={(id) => this.props.onPlaceRemoved(id)}
-                />  
-
+                />
+ 
             </View>
         );
     }
@@ -32,8 +21,7 @@ class FindPlaceScreen extends React.Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onPlaceRemoved: (key) => dispatch(deletePlace(key)),
-        onDeleteAll: () => dispatch(deleteAll())
+        onPlaceRemoved: (key) => dispatch(deletePlace(key))
     }
 }
 
