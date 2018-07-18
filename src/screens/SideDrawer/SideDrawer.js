c8f8cd55970ba7fadedff8ddc3d148214c1c4c22
rafael <nomar22@gmail.com>
2018-07-17 22:21:26 -0300

    tab styled

---
+++ src/screens/SideDrawer/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+class SideDrawer extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>SIDE Drawer</Text>
+            </View>
+        );
+    };
+};
+
+export default SideDrawer;
