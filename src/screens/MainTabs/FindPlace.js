affcfddd803f6a3e5e43da2cddfbdf076831c149
rafael <nomar22@gmail.com>
2018-07-18 09:40:07 -0300

    selecting place randomly

--- src/screens/MainTabs/FindPlace.js
+++ src/screens/MainTabs/FindPlace.js
@@ -3,32 +3,42 @@ import { View, Text, Button } from 'react-native';
 import { connect } from 'react-redux';
 import List from '../../components/List';
 import { deletePlace } from '../../store/actions/index';
-import { deleteAll } from '../../store/actions/places';
+import { deleteAll,makeChoice } from '../../store/actions/places';
 
 class FindPlaceScreen extends React.Component {
     itemSelectHandler = id => {
+        console.log('selecting')
         const place = this.props.places.find(({ key }) => key === id);
         this.props.navigator.push({
             screen: 'awesome-places.DetailScreen',
             passProps: {
                 selectedPlace: place
             },
-            title:place.placeName,
+            title: place.placeName, 
             animated: true,
             backButtonTitle: 'Back'
 
         });
     }
 
+    onChoiceForMe = () => {
+        this.props.onMakeChoice();
+        if(this.props.selectedPlace){
+            this.itemSelectHandler(this.props.selectedPlace.key);
+        }
+    }
+
 
     render() {
         return (
             <View>
-                {this.props.places.length > 0 ? (<Button
+                <Button
+                    title="Choice for me"
+                    onPress={this.onChoiceForMe} />
+                <Button
                     title="Delete All"
                     color="#841584"
-                    onPress={this.props.onPressDeleteAll} />)
-                : (null)}
+                    onPress={this.props.onPressDeleteAll} />
 
 
                 <List
@@ -39,19 +49,21 @@ class FindPlaceScreen extends React.Component {
 
             </View>
         );
-    }
+    } 
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onPlaceRemoved: (key) => dispatch(deletePlace(key)),
-        onPressDeleteAll: () => dispatch(deleteAll())
+        onPressDeleteAll: () => dispatch(deleteAll()),
+        onMakeChoice : ()=> dispatch(makeChoice())
     }
 }
 
 const mapStateToProps = state => {
     return {
-        places: state.places.places
+        places: state.places.places,
+        selectedPlace: state.places.selectedPlace
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(FindPlaceScreen);
