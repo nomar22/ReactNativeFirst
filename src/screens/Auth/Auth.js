6e3ce088d6ee45a2caf20a5eb765910c355ce28b
rafael <nomar22@gmail.com>
2018-07-31 15:06:20 -0300

    adding flexbox to auth page

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 
 
@@ -11,13 +11,26 @@ class AuthScreen extends Component {
     render() {
 
         return (
-            <View>
+            <View style={styles.container}>
                 <Text> Welcome </Text>
-                <Button title="Login" onPress={this.loginHandler} />
+                <Button  onPress={()=>{}} title = "Switch to Login " />
+                <TextInput placeholder="Email "/>
+                <TextInput placeholder="Password"/>
+                <TextInput placeholder="Confirm Password"/>
+                <Button onPress={()=>{}} title="Login" onPress={this.loginHandler} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1 ,
+        justifyContent:'center',
+        alignItems: 'center',
+    }
+
+});
 
 
 export default AuthScreen;
